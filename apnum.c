b9de00d0b6770317497dbaea3d63d62388315214
www <www@www.com>
2019-06-01 11:30:07 +0800

    x

--- apnum.c
+++ apnum.c
@@ -73,6 +73,85 @@ u32 APNUM_intToDecStr(const APNUM_int* x, char* decBuf, u32 decBufSize)
 
 
 
+void APNUM_intFromU8(APNUM_int* out, u8 x)
+{
+
+}
+
+
+void APNUM_intFromU16(APNUM_int* out, u16 x)
+{
+
+}
+
+
+void APNUM_intFromU32(APNUM_int* out, u32 x)
+{
+
+}
+
+
+void APNUM_intFromU64(APNUM_int* out, u64 x)
+{
+
+}
+
+
+
+
+
+void APNUM_intFromS8(APNUM_int* out, s8 x)
+{
+
+}
+
+
+void APNUM_intFromS16(APNUM_int* out, s16 x)
+{
+
+}
+
+
+void APNUM_intFromS32(APNUM_int* out, s32 x)
+{
+
+}
+
+
+void APNUM_intFromS64(APNUM_int* out, s64 x)
+{
+
+}
+
+
+
+
+
+void APNUM_intAdd(APNUM_int* out, const APNUM_int* a, const APNUM_int* b)
+{
+
+}
+
+
+
+void APNUM_intSub(APNUM_int* out, const APNUM_int* a, const APNUM_int* b)
+{
+
+}
+
+
+
+void APNUM_intMul(APNUM_int* out, const APNUM_int* a, const APNUM_int* b)
+{
+
+}
+
+
+
+void APNUM_intDiv(APNUM_int* out, const APNUM_int* a, const APNUM_int* b)
+{
+
+}
 
 
 
@@ -83,13 +162,35 @@ u32 APNUM_intToDecStr(const APNUM_int* x, char* decBuf, u32 decBufSize)
 
 
 
+bool APNUM_intGT(const APNUM_int* a, const APNUM_int* b)
+{
+    return false;
+}
 
 
 
 
 
+bool APNUM_intLT(const APNUM_int* a, const APNUM_int* b)
+{
+    return !APNUM_intGE(a, b);
+}
 
 
+
+
+bool APNUM_intGE(const APNUM_int* a, const APNUM_int* b)
+{
+    return false;
+}
+
+
+
+
+bool APNUM_intLE(const APNUM_int* a, const APNUM_int* b)
+{
+    return !APNUM_intGT(a, b);
+}
 
 
 
