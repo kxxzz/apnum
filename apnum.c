bc9be11ada0fe3cfcc86a28c3e5d1ed98f885086
www <www@www.com>
2019-06-01 16:05:33 +0800

    x

--- apnum.c
+++ apnum.c
@@ -47,25 +47,19 @@ u32 APNUM_intToDecStr(const APNUM_int* x, char* decBuf, u32 decBufSize)
 
 void APNUM_intFromU8(APNUM_int* out, u8 x)
 {
-
+    mpz_init_set_ui(&out->impl, x);
 }
-
-
 void APNUM_intFromU16(APNUM_int* out, u16 x)
 {
-
+    mpz_init_set_ui(&out->impl, x);
 }
-
-
 void APNUM_intFromU32(APNUM_int* out, u32 x)
 {
-
+    mpz_init_set_ui(&out->impl, x);
 }
-
-
 void APNUM_intFromU64(APNUM_int* out, u64 x)
 {
-
+    mpz_init_set_ui(&out->impl, x);
 }
 
 
@@ -74,26 +68,23 @@ void APNUM_intFromU64(APNUM_int* out, u64 x)
 
 void APNUM_intFromS8(APNUM_int* out, s8 x)
 {
-
+    mpz_init_set_si(&out->impl, x);
 }
-
-
 void APNUM_intFromS16(APNUM_int* out, s16 x)
 {
-
+    mpz_init_set_si(&out->impl, x);
 }
-
-
 void APNUM_intFromS32(APNUM_int* out, s32 x)
 {
-
+    mpz_init_set_si(&out->impl, x);
 }
-
-
 void APNUM_intFromS64(APNUM_int* out, s64 x)
 {
-
+    mpz_init_set_si(&out->impl, x);
 }
+
+
+
 
 
 
@@ -105,19 +96,16 @@ void APNUM_intAdd(APNUM_int* out, const APNUM_int* a, const APNUM_int* b)
 }
 
 
-
 void APNUM_intSub(APNUM_int* out, const APNUM_int* a, const APNUM_int* b)
 {
 
 }
 
 
-
 void APNUM_intMul(APNUM_int* out, const APNUM_int* a, const APNUM_int* b)
 {
 
 }
-
 
 
 void APNUM_intDiv(APNUM_int* out, const APNUM_int* a, const APNUM_int* b)
